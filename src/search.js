4300ef63fa7d9cf997b18b944b7698bd1d53cdf6
ana-vela <ana@anavela.net>
2021-03-11 21:06:47 -0700

    Pull all data

--- src/search.js
+++ src/search.js
@@ -1,60 +1,21 @@
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Search() {
+  const [data, setData] = useState(null);
 
-    const [query, setQuery] = useState("");
-    const [jobs, updateJobs] = useState([]);
+  useEffect(() => {
+    fetch(`/positions.json`)
+    .then((response) => response.json())
+    .then(setData);
+  }, []);
   
-    const searchJobs = async (e) => {
-      e.preventDefault();
-      let url = `/positions.json?description=${query}`;
-      fetch(url)
-        .then((response) => {
-          return response.json();
-        })
-        .then((res) => {
-          console.log(res.hits);
-          updateJobs(res.hits);          
-        })
-        .catch((err) => {
-          console.log("error", err);
-        });
-    };
-  
-    return (
-        <div>
-        <form className="form" onSubmit={searchJobs}>
-        <label className="label" htmlFor="query"></label>
-        <input 
-        className="input"
-        type="text"
-        name="query"
-        placeholder="search for jobs"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        />
-  <button className="button" type="submit">
-  Search
-  </button>
-        </form>
-        <div>
-        {jobs.map((job, i) => {
-          return (
-            <h2 key={i}>
-            {job.description}
-
-            </h2>
-          )
-        }
-
-          
-          )}
-        </div>
-        </div>
- 
-
-    )
+  if (data) {
+    return <div>{JSON.stringify(data)}</div>;
+  }
+  return (
+    <div>
+      no data available;
+    </div>
+  )
 }
-
-
