ce8d53e18a1dbdb961a74f633fa26c2a685d9953
ana-vela <ana@anavela.net>
2021-03-15 08:02:27 -0600

    add query functionality

--- src/search.js
+++ src/search.js
@@ -3,25 +3,27 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 
 export default function Search() {
-  const [data, setData] = useState();
+  const [data, setData] = useState({hits: []});
+  const [query, setQuery] = useState('');
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("/positions.json?description=javascript");
+      const result = await axios(`/positions.json?description=${query}`);
 
       setData(result.data);
+      console.log(result.data);
     };
     fetchData();
-  }, []);
+  }, [query]);
 
   let itemsToRender;
 
   if (data) {
     // eslint-disable-next-line no-unused-vars
-    itemsToRender = data.map((data) => {
+    itemsToRender = Object.values(data).map((data, index) => {
       return (
-        <div key={data.id}>
+        <div key={index}>
           <h1>{data.company}</h1>
           <div dangerouslySetInnerHTML={{ __html: data.description }} />;
         </div>
@@ -29,5 +31,19 @@ export default function Search() {
     });
   }
 
-  return <div>{itemsToRender}</div>;
+  return (
+  
+  <div>
+  <input 
+  type="text"
+  value={query}
+  onChange={event => setQuery(event.target.value)}
+  />
+  <button className="button" type="submit">
+              Search
+            </button>
+  
+  
+  {itemsToRender}</div>
+);
 }
